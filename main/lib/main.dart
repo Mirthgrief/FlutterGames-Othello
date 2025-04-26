d3794b05ce290f5a15eeb7d33a140ee824da430b
Mirthgrief <35170277+Mirthgrief@users.noreply.github.com>
2025-04-26 08:04:59 +0800

    Update main.dart

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -267,10 +267,16 @@ class _OthelloGameState extends State<OthelloGame> {
                   child: Center(
                     // Inside GridView.builder -> itemBuilder:
                     child: switch (board[row][col]) {
-                      //CellState.black => const Icon(Icons.circle, color: Colors.black, size: 36.0),
-                      //CellState.white => const Icon(Icons.circle, color: Colors.white, size: 36.0),
-                      CellState.black => SizedBox(width: 36,height: 36,child: Image.asset('assets/B_Piece.png'),),
-                      CellState.white => SizedBox(width: 36,height: 36,child: Image.asset('assets/W_Piece.png'),),
+                      CellState.black => SizedBox(
+                          width: 36,
+                          height: 36,
+                          child: Image.asset('assets/B_Piece.png'),
+                        ),
+                      CellState.white => SizedBox(
+                          width: 36,
+                          height: 36,
+                          child: Image.asset('assets/W_Piece.png'),
+                        ),
                       CellState.empty => null,
                     },
                   ),
